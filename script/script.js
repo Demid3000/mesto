b8b14f602269c8c9785097dceabf395e37c805db
Demid3000 <romantik14years@gmail.com>
2021-05-20 14:33:39 +0200

    4 проект

---
+++ script/script.js
@@ -0,0 +1,34 @@
+"use strict";
+let openPopup = document.querySelector('.popup-open');
+let popup = document.querySelector('.popup');
+let closePopup = popup.querySelector('.popup__close');
+
+openPopup.addEventListener('click', togglePopup);
+closePopup.addEventListener('click', togglePopup);
+function togglePopup(event){
+    popup.classList.toggle('popup_opened')
+}
+
+let i = 0;
+let like = document.querySelectorAll('.elements__like');
+for(i = 0; i < like.length; i++){
+    like[i].addEventListener('click', addLikeActive);
+}
+function addLikeActive(e){
+    if (e.target === e.currentTarget){
+        e.target.classList.toggle('elements__like_active');
+    }
+}
+
+let nameInput = document.querySelector('.popup__name');
+let jobInput = document.querySelector('.popup__text');
+let profileInfoName = document.querySelector('.profile__info_name');
+let profileInfoText = document.querySelector('.profile__info_text');
+let popupSubmitButton = popup.querySelector('.popup__submit-button');
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileInfoName.textContent = nameInput.value;
+    profileInfoText.textContent = jobInput.value;
+}
+popup.addEventListener('submit', formSubmitHandler); 
