71f17fa095821d0295bf9c76194bef46ca3c7d09
Demid3000 <romantik14years@gmail.com>
2021-05-23 00:14:57 +0200

    ща сдам отвечаю часть 8

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 "use strict";
-let openPopup = document.querySelector('.popup-open');
-let popup = document.querySelector('.popup');
+let openPopup = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('body > .popup');
 let closePopup = popup.querySelector('.popup__close');
 
 openPopup.addEventListener('click', togglePopup);
@@ -8,15 +8,14 @@ closePopup.addEventListener('click', togglePopup);
 function togglePopup(event){
     popup.classList.toggle('popup_opened')
 }
-
-let i = 0;
-let like = document.querySelectorAll('.elements__like');
+let i = 0; //без него лайки не робят... я хз почему 
+let like = document.querySelectorAll('.element__like');
 for(i = 0; i < like.length; i++){
     like[i].addEventListener('click', addLikeActive);
 }
 function addLikeActive(e){
     if (e.target === e.currentTarget){
-        e.target.classList.toggle('elements__like_active');
+        e.target.classList.toggle('element__like_active');
     }
 }
 
@@ -29,9 +28,11 @@ function formSubmitHandler (evt) {
     if(nameInput.value || jobInput.value){
         profileInfoName.textContent = nameInput.value;
         profileInfoText.textContent = jobInput.value;
+        return
     }else{
         profileInfoName.textContent != nameInput.value;
         profileInfoText.textContent != jobInput.value;
     }
+    togglePopup
 }
-popup.addEventListener('submit', formSubmitHandler); 
+popup.addEventListener('submit', formSubmitHandler); //popup это у меня и есть форма
