d0204860ef86a6c692837d4fdfe4574c0cbed2d3
Demid3000 <romantik14years@gmail.com>
2021-05-24 21:38:48 +0200

    ща сдам отвечаю часть 12

--- script/script.js
+++ script/script.js
@@ -6,7 +6,7 @@ let closePopup = popup.querySelector('.popup__close');
 openPopup.addEventListener('click', togglePopup);
 closePopup.addEventListener('click', togglePopup);
 function togglePopup(event){
-    popup.classList.toggle('popup_opened')
+    popup.classList.toggle('popup_opened');
 }
 // let i = 0; 
 // let like = document.querySelectorAll('.element__like');
@@ -22,8 +22,8 @@ function togglePopup(event){
 let popupForm = document.querySelector('.popup__form');
 let nameInput = document.querySelector('#name');
 let jobInput = document.querySelector('#job');
-let profileInfoName = document.querySelector('.profile__box-name');
-let profileInfoText = document.querySelector('.profile__box-text');
+let profileInfoName = document.querySelector('.profile__name');
+let profileInfoText = document.querySelector('.profile__text');
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileInfoName.textContent = nameInput.value; 
