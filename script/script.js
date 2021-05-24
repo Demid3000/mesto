a461af4df0fb4ba74afb8a88d27766ce7191ba9e
Demid3000 <romantik14years@gmail.com>
2021-05-24 19:12:04 +0200

    ща сдам отвечаю часть 11

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 "use strict";
 let openPopup = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('body > .popup');
+let popup = document.querySelector('.popup');
 let closePopup = popup.querySelector('.popup__close');
 
 openPopup.addEventListener('click', togglePopup);
@@ -8,32 +8,26 @@ closePopup.addEventListener('click', togglePopup);
 function togglePopup(event){
     popup.classList.toggle('popup_opened')
 }
-let i = 0; //без него лайки не робят... я хз почему 
-let like = document.querySelectorAll('.element__like');
-for(i = 0; i < like.length; i++){
-    like[i].addEventListener('click', addLikeActive);
-}
-function addLikeActive(e){
-    if (e.target === e.currentTarget){
-        e.target.classList.toggle('element__like_active');
-    }
-}
+// let i = 0; 
+// let like = document.querySelectorAll('.element__like');
+// for(i = 0; i < like.length; i++){
+//     like[i].addEventListener('click', addLikeActive);
+// }
+// function addLikeActive(e){
+//     if (e.target === e.currentTarget){
+//         e.target.classList.toggle('element__like_active');
+//     }
+// }
 
 let popupForm = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__job');
+let nameInput = document.querySelector('#name');
+let jobInput = document.querySelector('#job');
 let profileInfoName = document.querySelector('.profile__box-name');
 let profileInfoText = document.querySelector('.profile__box-text');
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    if(nameInput.value || jobInput.value){
-        profileInfoName.textContent = nameInput.value;
-        profileInfoText.textContent = jobInput.value;
-        return
-    }else{
-        profileInfoName.textContent != nameInput.value;
-        profileInfoText.textContent != jobInput.value;
-    }
-    togglePopup
+    profileInfoName.textContent = nameInput.value; 
+    profileInfoText.textContent = jobInput.value; 
+    togglePopup();
 }
 popupForm.addEventListener('submit', formSubmitHandler);
